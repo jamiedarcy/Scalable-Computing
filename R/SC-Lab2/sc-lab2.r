5a5ffa135d5182a9a3c4edc8e2e8efb134905475
jamiedarcy <jamie.darcy@live.com>
2016-09-27 16:40:47 +0100

    update

--- R/SC-Lab2/sc-lab2.r
+++ R/SC-Lab2/sc-lab2.r
@@ -1,62 +1,39 @@
+# Scalable Computing - Week 2 Lab
+# Loading Data Tables into R and Performing basic analysis
+# Jamie Darcy - 26/09/2016
+
+#Set Working Directory
 setwd('C:/Users/jdarcy/Documents/GitHub/Scalable-Computing/R')
 
+#read the file in
 file <- file.path("SC-Lab2","mpstat.txt")
-mydata <- read.table(file, 
-                     header = TRUE, 
-                     sep="\t")
+mydata <- read.table(file, header = TRUE)
 
-colnames(mydata)
-summary(mydata)
+#Print colnames from datafile
+print(colnames(mydata))
 
+#plot the idle time of this CPU
+plot(main="CPU Usage - Idle Time", mydata$X.idle)
+x11()
 
-str(mydata)
-print("hello")
-head(mydata)
-
-
-mean(mydata%Xidle)
-mean(mydata['%idle'])
-mean(mydata$%idle)
+#Plot the busy time of this CPU - (100 - idle)
+busytime <-100-mydata$X.idle
+plot(main="CPU Usage - Busy Time", busytime)
 
 
-
-
-
-
-#Plot the idle time of this CPU
-plot(mydata)
-#Plot the busy time of this CPU
-plot()
 #Find the min, max and mean of the data
-min(mydata)
-max(mydata)
-mean(mydata$idle)
+print(summary(busytime))
+#or print it out one by one
+cat("Mean busy time : ", mean(busytime))
+cat("Min busy time : ", min(busytime))
+cat("Max busy time : ", max(busytime))
+
+#boxplot data
+boxplot(busytime, main="CPU Usage") 
+
+#scaterplot data
+plot(busytime, mydata$X.idle, main="Scatterplot CPU Usage", 
+     xlab="Car Weight ", ylab="Miles Per Gallon ", pch=19) 
 
 
-# scatter plot  
-
-
-#Use a boxplot 
-boxplot()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#range(mydata)
-#diff(range(mydata)
-#summary(mydata)
-#summary(mydata)
+plot(density(busy))
