f8c83e508a4e2f919311232b96c8c56e06dbab3b
jamiedarcy <jamie.darcy@live.com>
2016-11-07 16:16:52 +0000

    Ca1

---
+++ C/ca1/pmatrix.c
@@ -0,0 +1,103 @@
+/*
+* Scalable Computing -CA1 
+* Jamie Darcy (X00130016) - 01/11/2016
+* to compile this file: g++ -fopenmp pmatrix.c -o pmatrix
+* to run this file:  ./pmatrix
+*/
+#include <iostream>
+#include <time.h>
+#include <omp.h>
+
+using namespace std;
+
+int main()
+{
+	/* DECLARING VARIABLES */
+	double wtime; // Execution time measure
+	int a[255][255], b[255][255], mult[255][255];   // declaring matrices of NxN size
+	int aRows, aColumns, bRows, bColumns, i, j, k;
+	srand(time(NULL)); //seeds the random number generator used by the function rand
+
+	cout << "\n";
+	cout << "The number of processors available = " << omp_get_num_procs() << "\n";
+
+	cout << "Enter the number of rows and columns for first matrix: ";
+	cin >> aRows >> aColumns;
+	cout << "Enter rows and columns for second matrix: ";
+	cin >> bRows >> bColumns;
+
+	/*
+	* If colum of first matrix in not equal to row of second matrix, 
+	* ask the user to enter the size of matrix again.
+	*/
+	while (aColumns != bRows)
+	{
+		cout << "Error! Multiplication is not possible of the two matrices. Try again \n.";
+		cout << "Enter rows and columns for first matrix: ";
+		cin >> aRows >> aColumns;
+		cout << "Enter rows and columns for second matrix: ";
+		cin >> bRows >> bColumns;
+	}
+
+	/* Record start time*/
+	wtime = omp_get_wtime();
+
+	/* Genarate matirix A with random number and print */
+	cout << endl << "Matrix A:" << endl;
+	for (i = 0; i<aRows; ++i)
+	{
+		for (j = 0; j<aColumns; ++j)
+		{
+			a[i][j] = rand() % 100;
+			cout << " " << a[i][j];
+		}
+		cout << "\n";
+	}
+
+	/* Genarate matirix B with random number and print */
+	cout << endl << "Matrix B:" << endl;
+	for (i = 0; i<bRows; ++i)
+	{
+		for (j = 0; j<bColumns; ++j)
+		{
+			b[i][j] = rand() % 100;
+			cout << " " << b[i][j];
+		}
+		cout << "\n";
+	}
+
+	/* Multiplication Logic Run in parallel
+	* Multiplying matrix a and b and storing in array mult. 
+	* Variable mult is shared accross processors while i, j, k are private
+	*/
+	#pragma omp parallel for shared(mult) private(i,j,k)
+	for (i = 0; i<aRows; ++i)
+		for (j = 0; j < bColumns; ++j)
+		{
+			mult[i][j] = 0;   // set initial value of resulting matrix = 0 
+			for (k = 0; k<aColumns; ++k)
+			{
+				mult[i][j] += a[i][k] * b[k][j];
+			}
+		}
+
+	/* Print the multiplication of two matrix. */
+	cout << endl << "Output Matrix: " << endl;
+	for (i = 0; i<aRows; ++i)
+		for (j = 0; j<bColumns; ++j)
+		{
+			cout << " " << mult[i][j];
+			if (j == bColumns - 1)
+				cout << endl;
+		}
+
+	/* Record End time*/
+	wtime = omp_get_wtime() - wtime;
+	cout << "  Elapsed seconds = " << wtime << "\n";
+	cout << "\n";
+
+	/* TERMINATE PROGRAM */
+	return 0;
+}
+
+
